fe3c2af05b285b904bb9dfb279d9e615ba9a6edd
Reynald Havard <reynald.havard@gmail.com>
2024-07-22 17:22:43 +0100

    generate parquet files and add audio utils

---
+++ src/audio_utils.py
@@ -0,0 +1,42 @@
+import librosa
+import numpy as np
+
+from typing import Tuple
+
+
+def load_audio(filename: str) -> Tuple[np.ndarray, int]:
+    y, sr = librosa.load(filename)
+
+    return y, sr
+
+
+def get_mfcc(
+    raw_audio: np.ndarray,
+    sample_rate: int,
+    n_mfcc: int = 13,
+    compute_delta: bool = True,
+    compute_delta2: bool = True,
+) -> np.ndarray:
+    if compute_delta2 and not compute_delta:
+        raise Exception("compute_delta needs to be set to True to use compute_delta2")
+
+    mfcc = librosa.feature.mfcc(y=raw_audio, sr=sample_rate, n_mfcc=n_mfcc)
+    features = [mfcc]
+
+    if compute_delta:
+        mfcc_delta = librosa.feature.delta(mfcc)
+        features.append(mfcc_delta)
+
+    if compute_delta2:
+        mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+        features.append(mfcc_delta2)
+
+    features = np.concatenate(features, axis=0).T
+
+    return features
+
+
+def normalize_mfcc(x: np.ndarray) -> np.ndarray:
+    x = (x - np.mean(axis=0)) / (x.std(axis=0 + 1e-8))
+
+    return x
--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,4 +1,10 @@
+from src.audio_utils import *
+
+import pandas as pd
+
 import os
+from tqdm import tqdm
+
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,7 +14,7 @@ def generate_training_list(
     output_list_file: str,
     validation_list_file: str,
     testing_list_file: str,
-):
+) -> None:
     validation_txt = os.path.join(CURRENT_DIR, data_folder, validation_list_file)
     testing_txt = os.path.join(CURRENT_DIR, data_folder, testing_list_file)
 
@@ -36,7 +42,7 @@ def generate_training_list(
             f.write(f"{filename}\n")
 
 
-def generate_labels(example_file: str, output_file: str):
+def generate_labels(example_file: str, output_file: str) -> None:
     with open(os.path.join(CURRENT_DIR, example_file)) as f:
         file_list = f.read().split("\n")[:-1]
 
@@ -46,14 +52,82 @@ def generate_labels(example_file: str, output_file: str):
         f.write("\n".join(labels))
 
 
+def generate_parquet_file(
+    data_list_file: str,
+    data_folder: str,
+    output_file: str,
+    n_mfcc: int = 13,
+    compute_delta: bool = False,
+    compute_delta2: bool = False,
+) -> None:
+    with open(os.path.join(CURRENT_DIR, data_folder, data_list_file)) as f:
+        data_list = f.read().split("\n")[:-1]
+
+    feature_dict = dict()
+    for filename in tqdm(data_list[:100]):
+        full_filename = os.path.join(CURRENT_DIR, data_folder, filename)
+        label = filename.split("/")[0]
+        raw_audio, sr = load_audio(full_filename)
+        mfcc = get_mfcc(
+            raw_audio=raw_audio,
+            sample_rate=sr,
+            n_mfcc=n_mfcc,
+            compute_delta=compute_delta,
+            compute_delta2=compute_delta2,
+        )
+        normalized_mfcc = normalize_mfcc(mfcc)
+        feature_dict[filename] = {"label": label, "normalized_mfcc": normalized_mfcc}
+
+    df = pd.DataFrame.from_dict(feature_dict, orient="index")
+
+    full_output_filename = os.path.join(CURRENT_DIR, data_folder, output_file)
+    df.to_parquet(full_output_filename)
+    print(f"Saved processed data to {full_output_filename}")
+
+
 if __name__ == "__main__":
+    DATA_FOLDER = "../data"
+    TRAINING_LIST = "training_list.txt"
+    VALIDATION_LIST = "validation_list.txt"
+    TESTING_LIST = "testing_list.txt"
+    TRAINING_FEATURES = "training_features.parquet"
+    VALIDATION_FEATURES = "validation_features.parquet"
+    TESTING_FEATURES = "testing_features.parquet"
+    LABELS_FILE = "../labels.txt"
+
     generate_training_list(
-        data_folder="../data",
-        output_list_file="training_list.txt",
-        validation_list_file="validation_list.txt",
-        testing_list_file="testing_list.txt",
+        data_folder=DATA_FOLDER,
+        output_list_file=TRAINING_LIST,
+        validation_list_file=VALIDATION_LIST,
+        testing_list_file=TESTING_LIST,
     )
 
     generate_labels(
-        example_file="../data/validation_list.txt", output_file="../labels.txt"
+        example_file=os.path.join(DATA_FOLDER, VALIDATION_LIST),
+        output_file=LABELS_FILE,
+    )
+
+    generate_parquet_file(
+        data_list_file=TRAINING_LIST,
+        data_folder=DATA_FOLDER,
+        output_file=TRAINING_FEATURES,
+        n_mfcc=13,
+        compute_delta=True,
+        compute_delta2=True,
+    )
+    generate_parquet_file(
+        data_list_file=VALIDATION_LIST,
+        data_folder=DATA_FOLDER,
+        output_file=VALIDATION_FEATURES,
+        n_mfcc=13,
+        compute_delta=True,
+        compute_delta2=True,
+    )
+    generate_parquet_file(
+        data_list_file=TESTING_LIST,
+        data_folder=DATA_FOLDER,
+        output_file=TESTING_FEATURES,
+        n_mfcc=13,
+        compute_delta=True,
+        compute_delta2=True,
     )
